b4050592e036b2879240f49aa9ef967378079f33
Sinha <musinha@paypalcorp.com>
2016-08-09 14:10:18 +0530

    Added CardUtils and game class

---
+++ src/CardUtils.java
@@ -0,0 +1,44 @@
+import java.util.Collections;
+import java.util.*;
+
+
+public class CardUtils {
+
+    public static boolean areSameColor(List<Card> cards) {
+        Card.Colors firstCardColor = cards.get(0).getColor();
+        for (Card card : cards) {
+            if (!(card.getColor() == firstCardColor)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean areSameSuite(List<Card> cards) {
+        Card.Suites firstCardSuite = cards.get(0).getSuite();
+        for (Card card : cards) {
+            if (!(card.getSuite() == firstCardSuite)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean areSequential(List<Card> cards) {
+        List<Integer> values = getFaceValues(cards);
+        Collections.sort(values);
+        for (int i = 0; i < values.size() - 1 ; i++){
+            if(values.get(i + 1) - values.get(i) != 1)
+                return false;
+        }
+        return true;
+    }
+
+    public static List<Integer> getFaceValues(List<Card> cards) {
+        List<Integer> values = new ArrayList<Integer>();
+        for (Card card : cards) {
+            values.add(card.getFace());
+        }
+        return values;
+    }
+}
