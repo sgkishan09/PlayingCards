40dd9436dd8f04abfc81390fc14e56b435b637c0
Katoch <rkatoch@paypalcorp.com>
2016-08-09 12:33:37 +0530

    Added Card class

---
+++ src/Card.java
@@ -0,0 +1,70 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Card {
+	
+	private static Map<String,Integer> NumericalValue;
+	
+	private int face;
+	
+	private Colors color;
+	
+	private Suites suite;
+	
+	
+	/*
+	 * 	Assessor functions
+	 * 
+	 */
+	protected int getFace() {
+		return this.face;
+	}
+	
+	protected Colors getColor() {
+		return this.color;
+	}
+	
+	protected Suites getSuite() {
+		return this.suite;
+	}
+	
+	/*
+	 * 	Enums for Suites and Colors
+	 * 
+	 */
+	
+	protected enum Suites {
+		SPADES, HEARTS, DIAMONDS, CLUBS
+	}
+	
+	protected enum Colors {
+		BLACK, RED
+	}
+		
+	private void createMap() {
+		NumericalValue = new HashMap<>();
+		NumericalValue.put("2",2);
+		NumericalValue.put("3",3);
+		NumericalValue.put("4",4);
+		NumericalValue.put("5",5);
+		NumericalValue.put("6",6);
+		NumericalValue.put("7",7);
+		NumericalValue.put("8",8);
+		NumericalValue.put("9",9);
+		NumericalValue.put("10",10);
+		NumericalValue.put("J",11);
+		NumericalValue.put("Q",12);
+		NumericalValue.put("K",13);
+		NumericalValue.put("A",14);
+		
+	}
+	
+	public Card (String Face, Suites suite) {
+		createMap();
+		this.face = NumericalValue.get(Face);
+		this.suite = suite;
+	}
+	
+}
