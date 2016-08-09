eba600e8b5d495a4f89566ccdc815540dbc01248
Katoch <rkatoch@paypalcorp.com>
2016-08-09 16:07:32 +0530

    Added constructor in Poker

--- src/Poker.java
+++ src/Poker.java
@@ -1,5 +1,11 @@
 class Poker extends Game {
 
+	public Poker(int noOfCards, int joker) {
+		deck = new Deck(0);
+		hand = new Hand(deck,noOfCards);
+		this.numberOfCards = noOfCards;
+	}
+	
 	@Override
 	void evaluate(Hand hand) {
 		
