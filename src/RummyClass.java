dba067166c865087fb62f7ad206f0a1e6c0eeb56
gkishan <gkishan@paypal.com>
2016-08-11 11:32:29 +0530

    Modified Deck and Hand

--- src/RummyClass.java
+++ src/RummyClass.java
@@ -70,16 +70,22 @@ public class RummyClass {
         return listToArray(origList);
     }
 
-    static List<List<String>> generateSplits(int arr[], int split[]) {
-        for (int i = 0; i < 1; i++) {
-            for (List<Integer> list : generateCombinations(arr, arr.length, split[i])) {
-                System.out.println(list);
-                int[] remainingCards = removeArray(arr, listToArray(list));
-                break;
-
+    static List<List<List<Integer>>> generateSplits(int arr[], int j, int split[]) {
+        List<List<List<Integer>>> listOfpossibleSplits = new ArrayList<>();
+        for (List<Integer> list : generateCombinations(arr, arr.length, split[j])) {
+            int[] remainingCards = removeArray(arr, listToArray(list));
+            if (j < split.length - 1) {
+                for (List<List<Integer>> splitsInAHand : generateSplits(remainingCards, j + 1, split)) {
+                    List<List<Integer>> local = new ArrayList<>();
+                    local.add(list);
+                    for (List<Integer> individualSplit : splitsInAHand)
+                        local.add(individualSplit);
+                    listOfpossibleSplits.add(local);
+                }
             }
+            break;
         }
-        return null;
+        return listOfpossibleSplits;
     }
 
     public static void main(String[] args) {
@@ -90,14 +96,13 @@ public class RummyClass {
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
         int r = 4;
         int n = arr.length;
-        int split[] = {4, 3, 3, 3};
+        List<Integer> split = Arrays.asList(new Integer[]{5, 4, 4});
         int s2[] = {4, 3, 3};
-        int test[] = removeArray(split, s2);
-        generateSplits(arr, split);
+        System.out.println(generateSplits(arr, 0, listToArray(split)));
         RummyClass rummy = new RummyClass();
         Deck deck = new Deck(2, 2);
         Hand hand = new Hand(13);
         hand.addCards(deck.pick(13));
-        System.out.println(hand);
+//        System.out.println(hand);
     }
 }
